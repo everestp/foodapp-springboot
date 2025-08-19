6735873b22c36596adb29e4c91c14aed50017740
Everest Paudel <145990676+everestp@users.noreply.github.com>
2025-08-19 11:55:17 +0545

    Update CartRepo.java

--- src/main/java/com/offnine/food_springboot/Repo/CartRepo.java
+++ src/main/java/com/offnine/food_springboot/Repo/CartRepo.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface CartRepo extends MongoRepository<CartEntity,String> {
-    Optional<CartEntity> findByUserId(String userId);
+    Optional<CartEntity> findByUserId(String userId); //
     void deleteByUserId(String userId);
 }
